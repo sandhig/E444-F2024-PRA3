562e181983203c150fb05d674ba4ce374cc78fba
sandhig <sandhi.ganjoo012@gmail.com>
2024-10-02 00:00:54 -0400

    set up Postgres

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+import os
 from pathlib import Path
 
 
@@ -11,7 +12,11 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URI = url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # create and initialize a new Flask app
